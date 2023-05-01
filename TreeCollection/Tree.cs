8e01edb41da6207ed96e22a30337908136ad293d
ladap1999 <ladap1999>
2023-05-01 10:51:57 +0300

    add classes Node, TreeEnumerator

--- TreeCollection/Tree.cs
+++ TreeCollection/Tree.cs
@@ -2,27 +2,63 @@
 
 namespace TreeCollection
 {
-    public class Tree<T> : IEnumerable<T> 
+    public class Tree<T> : IEnumerable<T>, IComparable<T>
     {
+        private Node<T> _root;
+        private bool _isReversed;
 
-        public Tree(bool isReversed = false) 
+        public Tree(bool isReversed = false)
         {
-            throw new NotImplementedException();
+            _root = null;
+            this._isReversed = isReversed;
         }
 
-        public void Add(T newElement)
+        public void Add(T value)
         {
-            throw new NotImplementedException();
+            _root = AddRecursive(_root, value);
+        }
+
+        private Node<T> AddRecursive(Node<T> current, T value)
+        {
+            if (current == null)
+            {
+                return new Node<T>(value);
+            }
+
+            if (Comparer<T>.Default.Compare(value, current.Value) == -1)
+            {
+                current.Left = AddRecursive(current.Left, value);
+            }
+            else if (Comparer<T>.Default.Compare(value, current.Value) == 1)
+            {
+                current.Right = AddRecursive(current.Right, value);
+            }
+            else
+            {
+                throw new Exception("An element with the same value already exists in the tree.");
+            }
+
+            return current;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            if (_isReversed)
+            {
+                return new TreeEnumerator<T>(_root, true);
+            }
+
+            return new TreeEnumerator<T>(_root, false);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+        
+        public int CompareTo(T? other)
+        {
+            return this.CompareTo(other);
         }
     }
 }
---
+++ TreeCollection/TreeEnumerator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+
+namespace TreeCollection;
+
+public class TreeEnumerator<T> : IEnumerator<T>
+{
+    private readonly bool isReversed;
+    private readonly Stack<Node<T>> stack = new Stack<Node<T>>();
+    private Node<T> current;
+    object IEnumerator.Current => Current;
+    public T Current
+    {
+        get
+        {
+            if (current == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return current.Value;
+        }
+    }
+
+    public TreeEnumerator(Node<T> root, bool isReversed)
+    {
+        this.isReversed = isReversed;
+        current = null;
+        Initialize(root);
+    }
+
+    private void Initialize(Node<T> node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        if (!isReversed)
+        {
+            Initialize(node.Right);
+            stack.Push(node);
+            Initialize(node.Left);
+        }
+        else
+        {
+            Initialize(node.Left);
+            stack.Push(node);
+            Initialize(node.Right);
+        }
+    }
+
+    public bool MoveNext()
+    {
+        if (stack.Count == 0)
+        {
+            return false;
+        }
+
+        current = stack.Pop();
+        return true;
+    }
+
+    public void Reset()
+    {
+        current = null;
+        stack.Clear();
+    }
+
+    public void Dispose()
+    {
+        current = null;
+        stack.Clear();
+    }
+}
